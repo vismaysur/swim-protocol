85b0233e94d7a761c95da663cea6f5de9f9ae5c7
Vismay-dev <vismaysuramwar@gmail.com>
2025-03-29 18:37:41 -0700

    Add gitignore

--- build/_deps/googletest-subbuild/CMakeFiles/Makefile.cmake
+++ build/_deps/googletest-subbuild/CMakeFiles/Makefile.cmake
@@ -10,8 +10,10 @@ set(CMAKE_MAKEFILE_DEPENDS
   "CMakeFiles/3.28.3/CMakeSystem.cmake"
   "CMakeLists.txt"
   "googletest-populate-prefix/tmp/googletest-populate-mkdirs.cmake"
+  "/opt/homebrew/Cellar/cmake/3.28.3/share/cmake/Modules/CMakeDetermineSystem.cmake"
   "/opt/homebrew/Cellar/cmake/3.28.3/share/cmake/Modules/CMakeGenericSystem.cmake"
   "/opt/homebrew/Cellar/cmake/3.28.3/share/cmake/Modules/CMakeInitializeConfigs.cmake"
+  "/opt/homebrew/Cellar/cmake/3.28.3/share/cmake/Modules/CMakeSystem.cmake.in"
   "/opt/homebrew/Cellar/cmake/3.28.3/share/cmake/Modules/CMakeSystemSpecificInformation.cmake"
   "/opt/homebrew/Cellar/cmake/3.28.3/share/cmake/Modules/CMakeSystemSpecificInitialize.cmake"
   "/opt/homebrew/Cellar/cmake/3.28.3/share/cmake/Modules/ExternalProject.cmake"
@@ -36,6 +38,7 @@ set(CMAKE_MAKEFILE_OUTPUTS
 
 # Byproducts of CMake generate step:
 set(CMAKE_MAKEFILE_PRODUCTS
+  "CMakeFiles/3.28.3/CMakeSystem.cmake"
   "googletest-populate-prefix/tmp/googletest-populate-mkdirs.cmake"
   "googletest-populate-prefix/src/googletest-populate-stamp/download-googletest-populate.cmake"
   "googletest-populate-prefix/src/googletest-populate-stamp/extract-googletest-populate.cmake"
